02efe8cb65e8888315b49f047410546a1347057b
GZanmiller <Rcfan0902@aol.com>
2017-04-10 01:22:58 -0400

    More site updates. Site is in semi-stable condition

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -14,21 +14,42 @@ $('a.aboutMenu').on('click', function(e) {
 
 	$("html,body").animate({
 		scrollTop: $(".about").offset().top - 66
-	}, 600);
+	}, 400);
 });
 $('a#workMenu').on('click', function(e) {
 	e.preventDefault();
 
 	$("html,body").animate({
 		scrollTop: $(".work").offset().top - 66
-	}, 600);
+	}, 400);
 });
 $('a#contactMenu').on('click', function(e) {
 	e.preventDefault();
 
 	$("html,body").animate({
 		scrollTop: $(".contact").offset().top - 66
-	}, 600);
+	}, 400);
 });
 
 /* ---------- Work Section ---------- */
+
+/* ---------- Contact Section ---------- */
+$('.contactForm').on('submit', function(e) {
+	e.preventDefault();
+
+	var data = {
+		name: $('#name').val(),
+		email: $('#email').val(),
+		message: $('#message').val()
+	}
+
+	$.post('/email.php', JSON.stringify(data), function(results) {
+		console.log(results);
+
+		if(results == 'true') {
+			$('.contactResultsTrue').show();
+		} else {
+			$('.contactResultsFalse').show();
+		}
+	});
+});
